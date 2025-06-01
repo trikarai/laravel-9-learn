d078056f405b927053ac789ed2b7549cff558c70
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-06-01 20:51:19 +0700

    feat: Add Bar and Foo classes with dependency injection test

---
+++ tests/Feature/DependencyInjectionTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Data\Bar;
+use App\Data\Foo;
+use Tests\TestCase;
+
+use function PHPUnit\Framework\assertEquals;
+
+class DependencyInjectionTest extends TestCase
+{
+
+    /**
+     * Test the dependency injection functionality within the application.
+     *
+     * @return void
+     */
+    public function testDepenjencyInjection()
+    {
+        $foo = new Foo();
+        $bar = new Bar($foo);
+ 
+        self:assertEquals('Foo and Bar', $bar->bar());
+    }
+}
+
