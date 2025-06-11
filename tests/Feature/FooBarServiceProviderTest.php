446b9613f81d20db0d791aa8fadabc3480fe0661
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-06-11 19:05:53 +0700

    feat: register class on service provider

---
+++ tests/Feature/FooBarServiceProviderTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Data\Bar;
+use App\Data\Foo;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class FooBarServiceProviderTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testServiceProvider()
+    {
+        $foo1 = $this->app->make(Foo::class);
+        $foo2 = $this->app->make(Foo::class);
+
+        self::assertSame($foo1, $foo2);
+
+        $bar1 = $this->app->make(Bar::class);
+        $bar2 = $this->app->make(Bar::class);
+
+        self::assertSame($bar1, $bar2);
+
+        self::assertSame($foo1, $bar1->foo);
+        self::assertSame($foo2, $bar2->foo);
+    }
+}
