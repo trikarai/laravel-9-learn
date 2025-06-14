fe48d64f7f9ff3ebf650bda0fbde733aefd95dbc
Tri Sutrisno <mas.trisutrisno@gmail.com>
2025-06-14 23:18:28 +0700

    test: route get

---
+++ tests/Feature/RouteTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RouteTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testHelloRoute()
+    {
+        $response = $this->get('/test');
+        $response->assertStatus(200);
+        $response->assertSeeText('Hello World!');
+    }
+}
